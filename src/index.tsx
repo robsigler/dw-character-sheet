a083b5e4de8927efc3ef111d6cd1b1ae109140b9
Robert Sigler <robert.b.sigler.mn@gmail.com>
2019-02-05 23:34:31 -0600

    Gather character sheet props into single object

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { CharacterSheet } from "./components/CharacterSheet";
+import { CharacterSheet, CharacterSheetProps } from "./components/CharacterSheet";
 
-const gearList = [{ name: "Adventuring Gear", weight: 1 }];
-const stats = {
-    strength: 10,
-    dexterity: 10,
-    constitution: 10,
-    intelligence: 10,
-    wisdom: 10,
-    charisma: 10,
+const characterSheetProps: CharacterSheetProps = {
+    name: "Helga",
+    gear: [{ name: "Adventuring Gear", weight: 1 }],
+    stats: {
+        strength: 10,
+        dexterity: 10,
+        constitution: 10,
+        intelligence: 10,
+        wisdom: 10,
+        charisma: 10,
+    },
 }
-ReactDOM.render(
-    <CharacterSheet name="Helga" stats={stats} gear={gearList} />,
+const stats = ReactDOM.render(
+    <CharacterSheet {...characterSheetProps} />,
     document.getElementById("app")
 );
