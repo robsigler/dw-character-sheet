99c9a80f73a5396915654b8c3816ab24ace87892
Robert Sigler <robert.b.sigler.mn@gmail.com>
2019-05-19 02:21:36 -0500

    Validate claim according to cognito developer guide

--- api/hello/auth/auth.go
+++ api/hello/auth/auth.go
@@ -18,9 +18,15 @@ type Header struct {
 	Alg string `json:"alg"`
 }
 
+type CognitoClaims struct {
+	TokenUse string `json:"token_use"`
+	jwt.StandardClaims
+}
+
 func GetAuthentication(r *http.Request) (string, error) {
 	authorization := r.Header.Get("Authorization")
 	sections := strings.Split(authorization, ".")
+	fmt.Println(sections)
 	headerString, err := jose.Base64Decode([]byte(sections[0]))
 	if err != nil {
 		return "", errors.New("Could not decode base64 from authorization token")
@@ -41,7 +47,7 @@ func GetAuthentication(r *http.Request) (string, error) {
 	if err != nil {
 		return "", errors.New("Failed to get private key")
 	}
-	claims := jwt.StandardClaims{}
+	claims := CognitoClaims{}
 	token, err := jwt.ParseWithClaims(authorization, &claims, func(token *jwt.Token) (interface{}, error) {
 		return publicKey, nil
 	})
@@ -52,9 +58,17 @@ func GetAuthentication(r *http.Request) (string, error) {
 		return "", errors.New("Token not valid")
 	}
 
-	// TODO: perform validation as described in https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html
+	if claims.Audience != "257dpg7bmbto3o1r20j9hqmcj9" {
+		return "", errors.New("Token not valid")
+	}
 
-	fmt.Println("aws_user_pools_web_client_id is", claims.Audience)
-	fmt.Println("User is authenticated as", claims.Subject)
+	if claims.Issuer != "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_VvaOhobD1" {
+		return "", errors.New("Token not valid")
+	}
+
+	if claims.TokenUse != "id" {
+		return "", errors.New("Token not valid")
+	}
+
 	return claims.Subject, nil
 }
