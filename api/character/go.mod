f8fab43aa4c5975a0cbfcb4dfcac5a0fa9f5bacb
Robert Sigler <robert.b.sigler.mn@gmail.com>
2019-05-30 01:05:35 -0500

    WIP save to dynamodb

--- api/character/go.mod
+++ api/character/go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/SermoDigital/jose v0.9.1
+	github.com/aws/aws-sdk-go v1.19.40
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/mendsley/gojwk v0.0.0-20141217222730-4d5ec6e58103
 )
--- api/character/index.go
+++ api/character/index.go
@@ -6,9 +6,15 @@ import (
 	"encoding/json"
 
 	"character/auth"
+
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 type CharacterState struct {
+	Id string `json:"id"`
 	Name string `json:"name"`
 	Stats Stats `json:"stats"`
 	Gear []ItemEntry `json:"gear"`
@@ -52,6 +58,55 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println("Request body:")
 	fmt.Println(body)
+	sheetJson, err := json.Marshal(body)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+	
+	svc := dynamodb.New(session.New())
+	input := &dynamodb.PutItemInput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"Id": {
+				S: aws.String(body.Id),
+			},
+			"Sheet": {
+				S: aws.String(string(sheetJson)),
+			},
+		},
+		TableName:              aws.String("dw-character-sheet-SheetTable-7SJTQH9CJTVG"),
+	}
+
+	result, err := svc.PutItem(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeConditionalCheckFailedException:
+				fmt.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
+			case dynamodb.ErrCodeProvisionedThroughputExceededException:
+				fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+			case dynamodb.ErrCodeResourceNotFoundException:
+				fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+			case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
+				fmt.Println(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
+			case dynamodb.ErrCodeTransactionConflictException:
+				fmt.Println(dynamodb.ErrCodeTransactionConflictException, aerr.Error())
+			case dynamodb.ErrCodeRequestLimitExceeded:
+				fmt.Println(dynamodb.ErrCodeRequestLimitExceeded, aerr.Error())
+			case dynamodb.ErrCodeInternalServerError:
+				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+	
+	fmt.Println(result)
 
 	fmt.Fprintf(w, "Hello! You have been identified as %s", user)
 }
